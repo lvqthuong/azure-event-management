36d5339944e0d33961ab2b5710440d1c783a24de
daysleeper23 <charnel1986@gmail.com>
2024-12-11 21:50:25 +0200

    Implement Service Bus message producing for event registration
    - Send a message (userId, eventId, status, timestamp) to Service Bus

--- src/main/java/com/example/fs19_azure/controller/EventsRegistrationsController.java
+++ src/main/java/com/example/fs19_azure/controller/EventsRegistrationsController.java
@@ -1,36 +1,65 @@
 package com.example.fs19_azure.controller;
 
+import com.example.fs19_azure.controller.response.GlobalResponse;
+import com.example.fs19_azure.dto.EventsRegistrationsMessage;
+import com.example.fs19_azure.entity.Events;
+import com.example.fs19_azure.entity.EventsRegistrationsStatus;
+import com.example.fs19_azure.exceptions.ErrorMessage;
+import com.example.fs19_azure.exceptions.GlobalException;
+import com.example.fs19_azure.repository.EventsRepository;
+import com.example.fs19_azure.service.ServiceBusClientService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.azure.messaging.servicebus.*;
 
+import java.time.Instant;
+import java.util.UUID;
+
+@Validated
 @RestController
-@RequestMapping("/events/register")
+@RequestMapping("/events/{id}/register")
 public class EventsRegistrationsController {
 
-    @Value("${SERVICE_BUS_CONNECTION_STRING}")
-    private String SERVICE_BUS_CONNECTION_STRING;
+    @Autowired
+    private EventsRepository eventsRepository;
 
-    @Value("${SERVICE_BUS_QUEUE_NAME}")
-    private String SERVICE_BUS_QUEUE_NAME;
-
+    @Autowired
+    private ServiceBusClientService serviceBusClientService;
 
     @PostMapping
-    public String register() {
-        ServiceBusSenderClient sender = new ServiceBusClientBuilder()
-            .connectionString(SERVICE_BUS_CONNECTION_STRING)
-            .sender()
-            .queueName(SERVICE_BUS_QUEUE_NAME)
-            .buildClient();
+    public ResponseEntity<GlobalResponse<String>> register(@Valid @PathVariable UUID id) throws Exception {
+        //check if the event exists
+        Events event = eventsRepository.findById(id).get();
+        if (event == null) {
+            throw new GlobalException(HttpStatus.NOT_FOUND, ErrorMessage.EVENT_NOT_FOUND);
+        }
 
+        //TODO: Implement the logic to check if the user is already registered for the event
 
-        // Send one message to the queue
-        String messageContent = "Event Registration Message";
-        sender.sendMessage(new ServiceBusMessage(messageContent).setMessageId("1"));
+        //send the registration message to the service bus
+        EventsRegistrationsMessage message = new EventsRegistrationsMessage(
+            "00000000-0000-0000-0000-000000000000"
+            , id.toString()
+            , EventsRegistrationsStatus.REGISTERED.name()
+            , Instant.now().toString()
+        );
+        serviceBusClientService.sendMessage(message);
 
-        return "Registered";
+        return new ResponseEntity<>(
+            new GlobalResponse<>(
+                HttpStatus.OK.value(), "Registration successful"
+            )
+            , HttpStatus.OK
+        );
     }
 
 }
