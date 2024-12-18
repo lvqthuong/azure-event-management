07c0a7822b2aaf90deec490011eca0332d7d0b36
daysleeper23 <charnel1986@gmail.com>
2024-12-18 13:05:02 +0200

    Finalize DELETE EventsAttachments

--- src/main/java/com/example/fs19_azure/repository/EventsAttachmentsRepository.java
+++ src/main/java/com/example/fs19_azure/repository/EventsAttachmentsRepository.java
@@ -4,8 +4,10 @@ import com.example.fs19_azure.entity.EventsAttachments;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface EventsAttachmentsRepository  extends JpaRepository<EventsAttachments, UUID> {
     List<EventsAttachments> findByEventId(UUID eventId);
+    Optional<EventsAttachments> findByEventIdAndId(UUID eventId, UUID id);
 }
