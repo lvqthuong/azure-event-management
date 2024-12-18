d63ad7c47d9866b28aedd158ddfca22e047aa53c
daysleeper23 <charnel1986@gmail.com>
2024-12-18 11:50:17 +0200

    Returning list of attachments for events in GET

--- src/main/java/com/example/fs19_azure/dto/UploadedAttachment.java
+++ src/main/java/com/example/fs19_azure/dto/UploadedAttachment.java
@@ -1,6 +1,7 @@
 package com.example.fs19_azure.dto;
 
 public record UploadedAttachment(
+    String id,
     String blobUrl,
     String blobName,
     String blobType,
--- src/main/java/com/example/fs19_azure/dto/mapper/EventsMapper.java
+++ src/main/java/com/example/fs19_azure/dto/mapper/EventsMapper.java
@@ -2,9 +2,13 @@ package com.example.fs19_azure.dto.mapper;
 
 import com.example.fs19_azure.dto.EventsRead;
 import com.example.fs19_azure.dto.EventsUpdate;
+import com.example.fs19_azure.dto.EventsWithAttachments;
+import com.example.fs19_azure.dto.UploadedAttachment;
 import com.example.fs19_azure.entity.Events;
 import org.mapstruct.*;
 import org.mapstruct.factory.Mappers;
+
+import java.util.List;
 
 @Mapper(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface EventsMapper {
@@ -20,6 +24,22 @@ public interface EventsMapper {
             event.getStartDate().toString(),
             event.getEndDate().toString(),
             event.getOrganizer().getId().toString(),
+            event.getMetadata(),
+            event.getUpdatedAt().toString(),
+            event.getCreatedAt().toString());
+    }
+
+    static EventsWithAttachments toEventsWithAttachments(Events event, List<UploadedAttachment> attachments) {
+        return new EventsWithAttachments(
+            event.getId().toString(),
+            event.getName(),
+            event.getDescription(),
+            event.getType(),
+            event.getLocation(),
+            event.getStartDate().toString(),
+            event.getEndDate().toString(),
+            event.getOrganizer().getId().toString(),
+            attachments,
             event.getMetadata(),
             event.getUpdatedAt().toString(),
             event.getCreatedAt().toString());
